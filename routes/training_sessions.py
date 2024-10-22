a93a9588f2ac7a7504dd95a0c872189e59dc40e6
SwimmingMantarain <swimmingmantarain@gmail.com>
2024-10-22 15:35:29 +0200

    made links for editing session work, will implement logic later

--- routes/training_sessions.py
+++ routes/training_sessions.py
@@ -1,26 +1,28 @@
-from flask import Blueprint, render_template, request
-from models import TrainingSession
-from flask_login import login_required
+from flask import Blueprint, render_template, request, redirect, url_for
+from models import TrainingSession, db
+from flask_login import login_required, current_user
 
 training_sessions = Blueprint('training_sessions', __name__)
-
-@training_sessions.route('/training_sessions', methods=['GET'])
-def get_training_sessions():
-    sessions = TrainingSession.query.all()
-    return render_template('partials/_training_sessions.html', sessions=sessions)
 
 @training_sessions.route('/create_session', methods=['GET', 'POST'])
 @login_required
 def create():
     if request.method == 'POST':
-        data = request.form
-        print(data)
+        name = request.form['session_name']
+        distance = request.form['session_distance']
+
+        new_session = TrainingSession(name=name, distance=distance, user_id=current_user.id)
+
+        db.session.add(new_session)
+        db.session.commit()
+
+        return redirect(url_for('user_profile.profile', user_name=current_user.username))
     return render_template('create_session.html')
 
-@training_sessions.route('/edit_session', methods=['GET', 'POST'])
+@training_sessions.route('/edit_session/<username>/<session_id>', methods=['GET', 'POST'])
 @login_required
-def edit():
-    return "<h1>edit Session</h1>"
+def edit(username, session_id):
+    return render_template('edit_session.html', username=username, session_id=session_id)
 
 @training_sessions.route('/view_sessions', methods=['GET'])
 @login_required
