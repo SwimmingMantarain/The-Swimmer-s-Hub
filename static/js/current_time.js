560bd7b3c1bd578a32ed46e387e419b3bfb3f788
SwimmingMantarain <SwimmingMantarain@gmail.com>
2024-11-22 10:14:44 +0000

    a fix to a bug :)

--- static/js/current_time.js
+++ static/js/current_time.js
@@ -4,6 +4,13 @@ window.onload = function() {
         var displayTime = date.toLocaleTimeString();
 
         var timeNoMeridiem = displayTime.replace(/( AM| PM)$/, '');
+        var timeParts = timeNoMeridiem.split(':');
+        var hours = Number(timeParts[0]);
+        var minutes = timeParts[1];
+        if (displayTime.includes('PM')) {
+            hours += 12;
+        }
+        timeNoMeridiem = hours + ':' + minutes;
 
         document.getElementById('date').value = displayDate;
         document.getElementById('time').value = timeNoMeridiem;
