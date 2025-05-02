c5c68cc332c4ec07a87e5a98fa7d5d65cd05ed44
SwimmingMantarain <SwimmingMantarain@gmail.com>
2025-05-02 14:11:23 +0200

    oooohp

--- routes/db_api.py
+++ routes/db_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from models import db, User
 from services import get_swimmer_id
@@ -8,13 +8,32 @@ db_api = Blueprint('db_api', __name__)
 @db_api.route("/db/post/find_swimrankings_id", methods=["POST"])
 @login_required
 def find_swimrankings_id():
-    print(request.form.items())
-    first_name = request.form['firstname']
-    last_name = request.form['lastname']
+    # Debug: Print all form data and request information
+    print("Form data:", request.form)
+    print("Content type:", request.content_type)
+    print("Data:", request.data)
 
-    id = get_swimmer_id(first_name, last_name)
+    try:
+        first_name = request.form.get('firstname')
+        last_name = request.form.get('lastname')
 
-    if id:
-        current_user.swimrankings_id = id
-    current_user.added_swimrankings = True
-    db.session.commit()
+        if not first_name or not last_name:
+            flash("First name and last name are required")
+            return redirect(url_for('user_profile.profile', user_name=current_user.username))
+
+        id = get_swimmer_id(first_name, last_name)
+
+        if id:
+            current_user.swimrankings_id = id
+            flash("Successfully found your Swimrankings ID!")
+        else:
+            flash("Could not find your Swimrankings ID. Please check your name.")
+
+        current_user.added_swimrankings = True
+        db.session.commit()
+
+        return redirect(url_for('user_profile.profile', user_name=current_user.username))
+
+    except Exception as e:
+        flash(f"An error occurred: {str(e)}")
+        return redirect(url_for('user_profile.profile', user_name=current_user.username))
